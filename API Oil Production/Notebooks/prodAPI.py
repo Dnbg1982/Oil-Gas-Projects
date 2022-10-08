29fde7acec2cb6a1f1c5b7540f42ce47e4fef713
Dnbg1982 <davidbanderasg@gmail.com>
2022-10-07 21:02:34 -0500

    commit

--- API Oil Production/Notebooks/prodAPI.py
+++ API Oil Production/Notebooks/prodAPI.py
@@ -1,9 +1,13 @@
 from tkinter import font
+from tokenize import group
 from sklearn.semi_supervised import LabelSpreading
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import plotly.express as px
 
 st.title ('Decay Well Production Predicted by Machine Learning')
 st.text('Data was gather from Kaggle Datasets')
@@ -33,3 +37,35 @@ st.line_chart(sum_produ)
 
 st.sidebar.title('Analytics')
 st.sidebar.radio('SHEETS',options=['Home','Plots', 'Tables','Forecasting'])
+
+st.markdown('### Forecasting Model')
+model = pickle.load(open(r'C:\Users\USUARIO\Documents\GitHub\Oil-Gas-Projects\API Oil Production\Notebooks\Oil_pred.pkl','rb'))
+
+def main():
+    API = st.text_input('Well_num')
+    Formation_ID = st.text_input('Formation')
+    Basin_ID = st.text_input('Basin')
+    Lateral_Length = st.text_input('Lateral_Length')
+    Month_Ord  = st.text_input('Month_After_Peak')
+
+    if st.button('Predict'):
+        makeprediction = model.predict(np.array([[API,Formation_ID, Basin_ID, Lateral_Length, Month_Ord]], dtype=object))
+        output = round(int(makeprediction[0]),2)
+        st.success(f'This well is going to produce around: {output} barrels in month {Month_Ord} after the peak')
+
+if __name__ == '__main__':  
+    main()    
+
+st.markdown('### Interactive Plot')
+
+wells = data.API.unique()[:20]
+
+data_f = data[(data.Month_Ord <= 36) & (data.API.isin(wells))]
+x_axis_op = data_f.Month_Ord
+y_axis_op = st.selectbox('Select Y-Axis', options=data_f.columns[[2,6]])
+groups = st.selectbox('Grouped', options=data_f.columns[7:9])
+
+plot = px.box(data_f, x=x_axis_op, y=y_axis_op, color=groups)
+st.plotly_chart(plot)
+
+data.mean()
